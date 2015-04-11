4968706f48a16d4e6d8f3c933576eef040e167d8
gunisalvo <gunisalvo@gmail.com>
2015-04-11 19:12:38 -0300

    mvn repo

--- pudim-api/src/main/java/org/entrementes/pudim/exception/ExceptionCode.java
+++
@@ -1,7 +0,0 @@
-package org.entrementes.pudim.exception;
-
-public enum ExceptionCode {
-	
-	UNMAPPED, WRAPPED, NOT_FOUND, CONFLICT,  BAD_KEY, BAD_REQUEST_BODY, CONFIGURATION_ERROR, UNAUTHORIZED;
-
-}
--- pudim-core/src/main/java/org/entrementes/pudim/events/EventInterceptor.java
+++
@@ -1,9 +0,0 @@
-package org.entrementes.pudim.events;
-
-public interface EventInterceptor<T> {
-
-	void handleEvent(T eventPayload);
-
-	void handleEvent(T eventPayload, Exception exception);
-
-}
